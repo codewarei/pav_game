9dcbd9321926c4ba4475f5d724e4047c1479472d
Xolani <sbongslany>
2022-05-13 11:12:01 +0200

    unused import

--- lib/game_view.dart
+++ lib/game_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pav_game/side_animation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class GameView extends StatefulWidget {
